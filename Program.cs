f1b885ebba686e9c9a30955c174654226f91eb52
Anthony Cherry <mrcherry97@gmail.com>
2022-06-03 12:27:52 -0400

    completed sntax

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Inferred Typing//
+ 
+var answer = 4;
+string response;
+
+if (answer < 9)
+{
+    Console.WriteLine(response = answer + "is less than nine");
+}
+else
+{
+    Console.WriteLine(response = answer + "greater than or equal to none");
+}
+
+
+// String Interpolation//
+
+/*var answer = 12;
+var response = "is less than nine or greater than nine";
+
+Console.WriteLine($"The {answer} answers she gave resulted in one of two response: {response}.");
+
+
+//Ternary Operator//
+
+var answer = 4;
+var response = (answer < 9) ? "Answer is less than nine." : "Answer is greater than nine.";
+
+Console.WriteLine(response);*/
